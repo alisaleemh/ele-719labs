0c3668382a87ca3d1aea53f9b73c07713f5bbce3
T. Ansari <talhairfan17@gmail.com>
2017-10-21 21:37:46 -0400

    Add files via upload

---
+++ Lab3/lab3demo3.1.py
@@ -0,0 +1,104 @@
+from numpy import array,dot
+from numpy.linalg import norm
+from math import sqrt, pi
+from time import time
+
+from myRobot import myRobot
+from Robot3WD.PID import PIDControllers
+
+# Set up sampling period and stopping time
+T_s =0.02
+T_f =8.5
+
+# Set up initial posture
+x_0 =0.0
+y_0 =0.0
+theta_0 = pi/6
+
+# Set up final posture
+x_f = 0.8
+y_f = 1.0
+theta_f = pi/2
+
+# set up p_0, p_f, delta_p
+p_0 = array([x_0, y_0, theta_0]).T
+p_f = array([x_f, y_f, theta_f]).T
+delta_p = (p_f - p_0)/T_f
+
+# Set up stopping criterion
+epsilon = sqrt( 2.0*(0.5/100.0)**2 + (2.0*pi/180.0)**2 )
+
+# Initialize PID controllers for the wheels
+Kp = array([1.0, 1.0, 1.0]).T
+Ki = array([1.0, 1.0, 1.0]).T
+Kd = array([0.01, 0.01, 0.01]).T
+pid = PIDControllers(Kp, Ki, Kd, T_s)
+
+# Initialize robot and other variables
+
+robot = myRobot(T_s)
+robot.initialize(theta_0)
+
+goal_reached = False
+
+current_time = 0.0
+
+# Set current posture p to p_0
+p = p_0
+
+# Calculate error norm
+error_norm = norm(p_f - p)
+
+# open a file to save data for plotting with MATLAB later
+file = open('p.csv', 'w')
+file.write('%10.3e %10.3e %10.3e %10.3e %10.3e\n' % (current_time, p[0], p[1], p[2], error_norm) ) 
+
+# Set start time
+start_time = time()
+
+# Control Loop
+while ( (not goal_reached) and (current_time < T_f) ): 
+   robot.get_readings_update()
+
+   # get current robot orientation
+   theta = robot.orientation
+
+   # Determine p_dot using forward kinematics
+   p_dot = robot.forward_kinematics(w, theta)
+
+   # Determine value of reference path p_r at current time
+   p_r = p_0 + delta_p*current_time
+
+   # Determine pd_dot (desired p_dot) using PID controllers
+   pd_dot = pid(p_r,p)
+
+   # Determine desired wheel velocities using inverse kinematics
+   w = robot.inverse_kinematics(pd_dot, theta)
+
+   # Execute the motion
+   robot.set_angular_velocities(w)
+
+   # odometry update
+   p = p + (p_dot*T_s)
+
+   # check to see if goal is reached
+   error_norm = norm(p_f - p)
+   goal_reached = (error_norm <= epsilon)
+
+   # time update
+   current_time = time() - start_time
+
+   # Write data to file
+   file.write('%10.3e %10.3e %10.3e %10.3e %10.3e\n' % (current_time, p[0], p[1], p[2], error_norm) ) 
+
+# Either goal is reached or current_time > T_f
+if goal_reached:
+   print ('Goal is reached, error norm is', error_norm)
+else:
+   print ('Failed to reach goal, error norm is', error_norm)
+
+robot.stop()
+robot.close()
+
+# written data to file 'p.csv'
+file.close()
---
+++ Lab3/myRobot.py
@@ -0,0 +1,63 @@
+from numpy import array, dot
+from math import sin, cos, exp, sqrt, pi
+
+from Robot3WD import Robot
+
+class myRobot(Robot):
+    def __init__(self, sampling_period):
+        Robot.__init__(self, sampling_period)
+
+    # --------------------------------------------------------------------------------------#
+    # Pre-Lab work for Experiment 2                                                         #
+    # --------------------------------------------------------------------------------------#
+    def inverse_kinematics(self, p_dot, theta):
+        L = self._L
+        wheel_radius = self._wheel_radius
+        m1_inv = (1.0/wheel_radius)*(array([[sin(theta), -cos(theta), -L], [cos((pi/6)+theta), sin((pi/6)+theta), -L], [-cos((pi/6)-theta), sin((pi/6)-theta), -L]]))
+        wheel_angular_velocities = dot(m1_inv,p_dot)
+	return wheel_angular_velocities
+    
+    def move_left(self, vx, theta):
+        p_dot = array([-vx, 0.0, 0.0]).T
+        w = self.inverse_kinematics(p_dot, theta)
+        self.set_angular_velocities(w)
+
+    def move_forward(self, vy, theta):
+        p_dot = array([0.0, vy, 0.0]).T
+        w = self.inverse_kinematics(p_dot, theta)
+        self.set_angular_velocities(w)
+        
+    def move_backward(self, vy, theta):
+        p_dot = array([0.0, -vy, 0.0]).T
+        w = self.inverse_kinematics(p_dot, theta)
+        self.set_angular_velocities(w)
+        
+    def move_right(self, vx, theta):
+        p_dot = array([vx, 0.0, 0.0]).T
+        w = self.inverse_kinematics(p_dot, theta)
+        self.set_angular_velocities(w)
+        
+    def rotate_CCW(self, w, theta):
+        p_dot = array([0.0, 0.0, w]).T
+        w = self.inverse_kinematics(p_dot, theta)
+        self.set_angular_velocities(w)
+        
+    def rotate_CW(self, w, theta):
+        p_dot = array([0.0, 0.0, -w]).T
+        w = self.inverse_kinematics(p_dot, theta)
+        self.set_angular_velocities(w)
+
+    # --------------------------------------------------------------------------------------#
+    # Pre-Lab work for Experiment 3                                                         #
+    # --------------------------------------------------------------------------------------#
+    def forward_kinematics(self, wheel_angular_velocities, theta):
+        L = self._L
+        wheel_radius = self._wheel_radius
+        m1 = (wheel_radius)*(array(([[sin(theta), -cos(theta), -L], [cos((pi/6)+theta), sin((pi/6)+theta), -L], [-cos((pi/6)-theta), sin((pi/6)-theta), -L]])^-1))
+        p_dot= dot(m1,wheel_angular_velocities)
+        return p_dot
+
+# end of myRobot class
+
+
+
